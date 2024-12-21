cc1a1dd92a019a107aa1b9102743ac2fe0661c28
Rahul-krx <krahul86946@gmail.com>
2024-12-21 14:24:16 +0530

    upload

---
+++ Main/menu.js
@@ -0,0 +1,4 @@
+let menu = document.querySelector('.navbar');
+document.querySelector('#menu-icon').onclick = () =>{
+    menu.classList.toggle('open-menu')  ;  // toggle class for show/hide menu
+ }
---
+++ server.js
@@ -0,0 +1,70 @@
+import express from 'express';
+import dotenv from "dotenv";
+import Stripe from 'stripe';
+
+// load variables.....
+
+dotenv.config();
+
+// start server....
+const app = express();
+
+app.use(express.static("Main"));
+app.use(express.json());
+
+// Home router.....
+
+app.get("/", (req , res) =>{
+    res.sendFile("index.html", {root:"Main"});
+})
+// Cart....
+app.get("/cart.html", (req, res) => {
+    res.sendFile("cart.html", {root: "Main"});
+});
+// success payment.....
+app.get("/success.html", (req, res) => {
+    res.sendFile("success.html", {root: "Main"});
+});
+// failed payment.....
+app.get("/cancel.html", (req, res) => {
+    res.sendFile("cancel.html", {root: "Main"});
+});
+
+// stripe......
+
+let stripeGateway = Stripe(process.env.stripe_key);
+app.post("/stripe-checkout", async(req, res)=>{
+    const lineItems = req.body.items.map((item) =>{
+        const unitAmount = parseInt(parseFloat(item.price)* 100)
+        console.log("item-price:", item.price);
+        console.log("unitAmount:", unitAmount);
+        return{
+            price_data: {
+                currency: "usd",
+                product_data: {
+                    name: item.title,
+                    images: [item.image],                  
+                },
+                unit_amount: unitAmount,
+            },
+            quantity : item.quantity,
+        }
+    })
+    const session = await stripeGateway.checkout.sessions.create({
+        payment_method_types: ["card"],
+        mode: "payment",
+        success_url : `http://localhost:3000/success.html`,
+        cancel_url : `http://localhost:3000/cancel.html`,
+        billing_address_collection : "required",
+        line_items: lineItems,
+    });
+    res.json({url: session.url});
+});
+
+
+
+
+app.listen(3000, () =>{
+    console.log("Server is running on port 3000");
+})
+
